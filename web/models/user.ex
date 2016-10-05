52c07364988357e8fbe018ac1dceafe47467fc6a
Abdullah <Abdullah.dahmash@yahoo.com>
2016-10-05 16:14:45 +0300

    init

---
+++ web/models/user.ex
@@ -0,0 +1,21 @@
+defmodule PhoenixTrello.User do
+  use PhoenixTrello.Web, :model
+
+  schema "users" do
+    field :first_name, :string
+    field :last_name, :string
+    field :email, :string
+    field :encrypted_password, :string
+
+    timestamps()
+  end
+
+  @doc """
+  Builds a changeset based on the `struct` and `params`.
+  """
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, [:first_name, :last_name, :email, :encrypted_password])
+    |> validate_required([:first_name, :last_name, :email, :encrypted_password])
+  end
+end
